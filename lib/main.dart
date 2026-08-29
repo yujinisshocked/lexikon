1413a154732044c3e67f136d07725d7913d3a7fb
Yujin <your-email@example.com>
2026-08-29 11:41:50 +0800

    Initialized hive_ce

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:lexikon/mainview.dart';
 
-void main() {
-  runApp(Lexikon());
+void main() async {
+  // Initialize Hive
+  await Hive.initFlutter();
+
+  // Open Hive boxes
+  await Hive.openBox('LEXIKON_SETTINGS');
+  await Hive.openBox('LEXIKON_NOTES');
+  await Hive.openBox('LEXIKON_HABITS');
+  await Hive.openBox('LEXIKON_TODOLIST');
+  await Hive.openBox('LEXIKON_SHOPPING');
+  await Hive.openBox('LEXIKON_BUDGET');
+  await Hive.openBox('LEXIKON_FINANCIAL');
+  runApp(const Lexikon());
 }
 
 class Lexikon extends StatelessWidget {
-  const new({super.key});
+  const Lexikon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MainView());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MainView(),
+    );
   }
 }
+
